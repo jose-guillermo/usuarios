903d8a72c6b539fae3a1ed88158101bc98e62fb4
José Guillermo Bejar Ccama <joseguille.jbc@gmail.com>
2022-03-10 13:30:22 +0100

    aplicacion sin funciones

---
+++ README.md
@@ -0,0 +1,8 @@
+https://randomuser.me/api
+Cargamos 10 usuarios chicas con nombre, foto, email, coordenadas, de longitud, mapa pequeñito
+
+https://maps.googleapis.com/maps/api/staticmap?parameters
+
+https://maps.googleapis.com/maps/api/staticmap?center=Berkeley,CA&zoom=14&size=400x400&key=YOUR_API_KEY
+
+Un boton que cargue lo mismo con 10 chicos
---
+++ main.js
@@ -0,0 +1,41 @@
+var display = document.getElementById("display");
+fetch("https://randomuser.me/api?gender=female&&results=10")
+    .then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        var personas = data.results;
+        console.log(personas);
+        for (var persona of personas) {
+            var registro = `
+    <tr>
+        <td>${persona.name.first} ${persona.name.last}</td>
+        <td><img src='${persona.picture.thumbnail}'></td>
+        <td>${persona.email} </td>
+        <td>${persona.location.coordinates.latitude} </td>
+        <td>${persona.location.coordinates.longitude}</td>
+    </tr>
+    `;
+            display.innerHTML += registro;
+        }
+    }).catch(function (err) {
+        console.log(err);
+    })
+// var display = document.getElementById("display");
+// fetch("https://randomuser.me/api?gender=female&&results=10")
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         var personas = data.results;
+
+//         console.log(personas);
+//         for(var persona of personas){
+//             var registro = '
+//             <tr>
+//             <td>${person.name.first} ${persona.name.last}</td>
+//             </tr>';
+//            }
+//            display.innerHTML += registro;
+//     }).catch(function (err) {
+//         console.log(err);
+//     });
